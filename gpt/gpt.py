9d29e7654dbce75c27dab167f87aa6bcc67fc3ca
towelsh <tea@towel.sh>
2023-11-25 00:11:26 +0000

    add configurable model

--- gpt/gpt.py
+++ gpt/gpt.py
@@ -3,19 +3,19 @@ from discord import Message
 from redbot.core.commands import Context
 from redbot.core.bot import Red
 import logging
-from openai._client import AsyncOpenAI, OpenAIError
+from openai._client import AsyncOpenAI
+from openai._exceptions import OpenAIError, NotFoundError
 
 class GPT(commands.Cog):
     def __init__(self, bot: Red):
         self.logger = logging.getLogger("GPT")
         self.bot: Red = bot
         self.client: AsyncOpenAI | None = None
-        self.config = Config.get_conf(self, identifier=124987269)
-        self.config.register_guild(prompt="You are a helpful assistant.")
+        self.config = Config.get_conf(self, identifier=124987269) 
+        self.config.register_guild(prompt="You are a helpful assistant.", model="gpt-3.5-turbo")
 
     async def _get_client(self) -> AsyncOpenAI:
         return AsyncOpenAI(api_key=(await self.bot.get_shared_api_tokens("openai")).get("api"))
-
 
     @commands.command()
     async def chat(self, ctx: Context, *args):
@@ -29,22 +29,29 @@ class GPT(commands.Cog):
                 return
 
         prompt: str = "You are a helpful assistant."
+        model = "gpt-3.5-turbo"
         if ctx.guild is not None:
             prompt = await self.config.guild(ctx.guild).prompt()
+            model = await self.config.guild(ctx.guild).model()
 
         async with ctx.typing():
             self.logger.info(f"{ctx.author} asks '{query}' in guild {ctx.guild}")
-            comp = await self.client.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=[{
-                    "role": "system",
-                    "content": prompt
-                }, {
-                    "role": "user",
-                    "content": query
-                }]
-            )
-        await ctx.reply(comp.choices[0].message.content)
+            try:
+                comp = await self.client.chat.completions.create(
+                    model=model,
+                    messages=[{
+                        "role": "system",
+                        "content": prompt
+                    }, {
+                        "role": "user",
+                        "content": query
+                    }]
+                )
+                await ctx.reply(comp.choices[0].message.content)
+            except NotFoundError as e:
+                self.logger.error(f"OpenAI API returned a NotFoundError. Check that completions model '{model}' is valid. Otherwise, create an issue on GitHub.")
+                self.logger.exception(e)
+                await ctx.reply(f"Unable to send to OpenAI. Please contact the owner of this bot.\n`help: check completions model '{model}' is valid (further details logged)`")
 
     @commands.Cog.listener()
     async def on_message(self, message: Message):
@@ -74,28 +81,35 @@ class GPT(commands.Cog):
                 self.logger.info(f"{message.author} asks '{message.content}' in guild {message.guild} in reply to {message.reference}")
 
                 prompt: str = "You are a helpful assistant."
+                model = "gpt-3.5-turbo"
                 if message.guild is not None:
                     prompt = await self.config.guild(message.guild).prompt()
+                    model = await self.config.guild(message.guild).model()
 
                 assert replied_to.reference is not None
                 assert replied_to.reference.message_id is not None
-                comp = await self.client.chat.completions.create(
-                    model="gpt-3.5-turbo",
-                    messages=[{
-                        "role": "system",
-                        "content": prompt
-                    }, {
-                        "role": "user",
-                        "content": (await message.channel.fetch_message(replied_to.reference.message_id)).content
-                    }, {
-                        "role": "assistant",
-                        "content": replied_to.content
-                    }, {
-                        "role": "user",
-                        "content": message.content
-                    }]
-                )
-            await message.reply(comp.choices[0].message.content)
+                try:
+                    comp = await self.client.chat.completions.create(
+                        model=model,
+                        messages=[{
+                            "role": "system",
+                            "content": prompt
+                        }, {
+                            "role": "user",
+                            "content": (await message.channel.fetch_message(replied_to.reference.message_id)).content
+                        }, {
+                            "role": "assistant",
+                            "content": replied_to.content
+                        }, {
+                            "role": "user",
+                            "content": message.content
+                        }]
+                    )
+                    await message.reply(comp.choices[0].message.content)
+                except NotFoundError as e:
+                    self.logger.error(f"OpenAI API returned a NotFoundError. Check that completions model '{model}' is valid. Otherwise, create an issue on GitHub.")
+                    self.logger.exception(e)
+                    await message.reply(f"Unable to send to OpenAI. Please contact the owner of this bot.\n`help: check completions model '{model}' is valid (further details logged)`")
 
     @commands.admin()
     @commands.command()
@@ -116,3 +130,22 @@ class GPT(commands.Cog):
             await ctx.reply("You must run this command from within a server!")
             return
         await ctx.reply(f"Current system prompt: {await self.config.guild(ctx.guild).prompt()}")
+
+    @commands.admin()
+    @commands.command()
+    async def setmodel(self, ctx: Context, model: str):
+        if ctx.guild is None:
+            await ctx.reply("You must run this command from within a server!")
+            return
+
+        await self.config.guild(ctx.guild).model.set(model)
+        await ctx.reply(f"Model was set to {model}")
+
+    @commands.admin()
+    @commands.command()
+    async def getmodel(self, ctx: Context):
+        if ctx.guild is None:
+            await ctx.reply("You must run this command from within a server!")
+            return
+        await ctx.reply(f"Current model: {await self.config.guild(ctx.guild).model()}")
+
